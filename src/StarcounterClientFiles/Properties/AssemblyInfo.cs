42e512814372ba24717eff3b3cb516ac3941e97a
Tomek Wytrębowicz <tomalecpub@gmail.com>
2018-01-05 18:50:06 +0100

    2.1.1

--- src/StarcounterClientFiles/Properties/AssemblyInfo.cs
+++ src/StarcounterClientFiles/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using Starcounter.Internal;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.1")]
+[assembly: AssemblyFileVersion("2.1.1")]
 
 // Assures the current assembly has a reference to the Starcounter
 // assembly. A reference to Starcounter is currently required for
