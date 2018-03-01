37db4d2fb066c7b93419ddae6db27cec4403ad23
Omar Alshaker <omar@omaralshaker.com>
2018-03-01 21:22:31 +0100

    3.0.1

--- src/StarcounterClientFiles/Properties/AssemblyInfo.cs
+++ src/StarcounterClientFiles/Properties/AssemblyInfo.cs
@@ -33,11 +33,11 @@ using Starcounter.Internal;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("3.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.1")]
 
 // this doesn't support semver, that's why it's 3.0.0, we can delete this release when we release actual 3.0.0
-[assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: AssemblyVersion("3.0.1")]
+[assembly: AssemblyFileVersion("3.0.1")]
 
 // Assures the current assembly has a reference to the Starcounter
 // assembly. A reference to Starcounter is currently required for
