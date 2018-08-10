656a8aa50bbd5e82c7d7f156de37e6f27104e6fb
Omar Alshaker <omar@omaralshaker.com>
2018-08-10 15:33:14 +0200

    Automate pre-cache service worker creation

---
+++ src/StarcounterClientFiles/create-service-worker.js
@@ -0,0 +1,85 @@
+const find = require('find');
+const path = require('path');
+const fs = require('fs');
+const prettyBytes = require('pretty-bytes');
+const { hashElement } = require('folder-hash');
+
+const BLACK_LISTED = [
+  'node_modules',
+  'polymer-source',
+  'test',
+  'tests',
+  'gruntfile.js',
+  'index.html',
+  'demo',
+  'demos',
+  'bower_components',
+  'app-shell',
+  'test_coverage'
+];
+function log(msg) {
+  console.log('create-service-worker: ' + msg);
+}
+const hashOptions = {
+  folders: { exclude: BLACK_LISTED },
+  files: { include: ['*.js', '*.html', '*.css'] }
+};
+
+function fileFilter(file) {
+  const pathComponents = file.split(path.sep);
+  if (
+    pathComponents.some(component =>
+      BLACK_LISTED.includes(component.toLowerCase())
+    )
+  ) {
+    return false;
+  }
+  return true;
+}
+
+function groomFile(file) {
+  file = path.relative(path.resolve(__dirname, 'wwwroot', 'sys'), file);
+  file = file.replace(/\\/g, '/');
+  return file;
+}
+
+function calcTotalSize(files) {
+  return files.reduce((acc, file) => acc + fs.statSync(file).size, 0);
+}
+
+async function createServiceWorker(filesJsonArray) {
+  const template = fs
+    .readFileSync(path.resolve(__dirname, 'service-worker.tmpl.js'))
+    .toString();
+  const hash = (await hashElement('.', hashOptions)).hash;
+  const serviceWorkerCode = template
+    .replace('REPLACE_ME_WITH_WITH_PRE_CACHED_FILES_HASH', `'${hash}'`)
+    .replace('REPLACE_ME_WITH_WITH_PRE_CACHE_URLS', filesJsonArray);
+  fs.writeFileSync(
+    path.resolve(__dirname, 'wwwroot', 'service-worker.js'),
+    serviceWorkerCode
+  );
+  return {hash, path: path.resolve(__dirname, 'wwwroot', 'service-worker.js')};
+}
+
+log('Look for JS, HTML and CSS files...');
+
+find.file(
+  /\.js$|\.html$|\.css$/,
+  path.resolve(__dirname, 'wwwroot', 'sys'),
+  function(files) {
+    log(`Found a total of ${files.length} file, pruning black-listed files`);
+    files = files.filter(fileFilter);
+    log(`File count after pruning is ${files.length}`);
+    const totalSize = calcTotalSize(files);
+    log(`Total size of all files ${prettyBytes(totalSize)}`);
+    files = files.map(groomFile);
+    const array = JSON.stringify(files);
+    log(`Hydrating service worker template...`);
+    createServiceWorker(array).then(result => {
+      log('Service worker created! Cache hash is: ' + result.hash);
+      log('Service worker saved at: ' + result.path);
+      log('Done');
+    });
+  }
+);
---
+++ src/StarcounterClientFiles/service-worker.tmpl.js
@@ -0,0 +1,111 @@
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+const PRECACHE = REPLACE_ME_WITH_WITH_PRE_CACHED_FILES_HASH;
+const RUNTIME = 'runtime';
+
+// A list of local resources we always want to be cached.
+const PRECACHE_WITHOUT_SYS = REPLACE_ME_WITH_WITH_PRE_CACHE_URLS;
+
+/* there is a couple hundred URLs all prefixed with `sys`. Removing those `sys`es and adding them here saves ~1KB */
+const PRECACHE_URLS = PRECACHE_WITHOUT_SYS.map(url => `sys/${url}`);
+
+
+function isRequestForAnAsset(url) {
+  const lastPartOfUrl = url.split('/').pop();
+  if (lastPartOfUrl.match(/\.\w{2,5}$/)) {
+    // has an extenstion
+    return true;
+  }
+  if (url.includes('htmlmerger')) {
+    // an HTML merger url
+    return true;
+  }
+  return false;
+}
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames => {
+        return cacheNames.filter(
+          cacheName => !currentCaches.includes(cacheName)
+        );
+      })
+      .then(cachesToDelete => {
+        return Promise.all(
+          cachesToDelete.map(cacheToDelete => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    const acceptHeader = event.request.headers.get('accept');
+
+    // skip Palindrom objects
+    if (acceptHeader && acceptHeader.includes('application/json')) {
+      return;
+    }
+
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request, {
+            headers: { Accept: acceptHeader } // forward headers
+          }).then(response => {
+            // only cache assets
+            // caching non-assets would cache eg MainPage (which is technically the app shell),
+            // and would cache REST API calls
+            if (isRequestForAnAsset(event.request.url)) {
+              console.log('cached', event.request.url)
+              return cache.put(event.request, response.clone()).then(() => {
+                return response;
+              });
+            } else {
+              console.log('skipped', event.request.url)
+              return response;
+            }
+          });
+        });
+      })
+    );
+  }
+});
